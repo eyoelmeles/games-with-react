9b964a1405446f2c33a2c9e65ca788d57684a78b
eyoelmeles <tojoelmeles@gmail.com>
2022-12-06 23:49:28 -0800

    Elements drop down to the bottom and player turns

--- connect-4/src/App.tsx
+++ connect-4/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
@@ -6,15 +6,28 @@ const ROWS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const COLS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 function App() {
   const [turn, setTurn] = useState<'black' | 'white'>('black');
+  const [filled, setFilled] = useState<string[]>([]);
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
+
+  useEffect(() => {}, [filled]);
 
   function handleHover(index: number) {
     setHoverIndex(index);
   }
-
-  function handleLogic(index: number) {
-    alert(index);
+  function handleLogic(e: any, index: number) {
     // TODO for the game logic
+    for (let i = ROWS.length; i >= 1; i--) {
+      if (
+        filled.includes(`${i},${e.target.attributes['data-col'].nodeValue}`)
+      ) {
+        continue;
+      } else {
+        filled.push(`${i},${e.target.attributes['data-col'].nodeValue}`);
+        console.log('FILLED', filled);
+        break;
+      }
+    }
+    setTurn((prevTurn) => (prevTurn === 'black' ? 'white' : 'black'));
   }
 
   return (
@@ -27,13 +40,22 @@ function App() {
             key={col.toString()}
             className={`column ${index === hoverIndex ? 'col-hover' : ''}`}
             onMouseOver={() => handleHover(index)}
-            onClick={() => handleLogic(index + 1)}
+            onClick={(e) => handleLogic(e, index + 1)}
           >
             {ROWS.map((row) => (
               <p
                 key={`${row.toString() + col}`}
-                className="cels"
-                id={`${row.toString() + col}`}
+                className={`cels ${
+                  filled.includes(row + ',' + col)
+                    ? turn === 'white'
+                      ? 'filled-blue'
+                      : 'filled-red'
+                    : ''
+                }`}
+                data-col={col}
+                data-row={row}
+                data-filled={filled.includes(`${row},${col}}`)}
+                id={`${row},${col}`}
               ></p>
             ))}
           </div>
